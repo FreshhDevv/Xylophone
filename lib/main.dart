2fd542e3457feccfc5f8665e0b8ee0cb5c940016
FreshDev <billyhans90@gmail.com>
2022-07-10 14:43:36 +0100

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,8 @@ import 'package:assets_audio_player/assets_audio_player.dart';
 void main() {
   runApp(const XylophoneApp());
 }
-
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
-
-  // Future<void> setSource(Source source) async {
-  //   setState(() => isSourceSet = false);
-  //   await widget.player.setSource(source);
-  //   setState(() => isSourceSet = true);
-  // }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,14 +16,10 @@ class XylophoneApp extends StatelessWidget {
             child: TextButton(
               onPressed: () {
                 final assetsAudioPlayer = AssetsAudioPlayer();
-
                 assetsAudioPlayer.open(
                   Audio("assets/audio/note1.wav"),
                 );
               },
-              // onPressed: () => AudioCache AudioCache({String prefix = 'assets/note1.wav'}),
-              //     String prefix;
-
               child: const Text('Click Me'),
             ),
           ),
